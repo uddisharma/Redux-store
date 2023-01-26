59f99800e3d08122b29b45e9c35b03e65ee184cc
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-01-26 23:38:21 +0530

    redux

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,38 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import { add } from '../store/CartSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../store/ProductSlice';
+import { STATUSES } from '../store/ProductSlice';
 function Products() {
+    const { data, status } = useSelector((state) => state.product)
     const dispatch = useDispatch();
-    const [prod, setProd] = useState([]);
+    // const [prod, setProd] = useState([]);
     useEffect(() => {
-        const fetchprods = async () => {
-            const res = await fetch('https://fakestoreapi.com/products')
-            const data = await res.json();
-            console.log(data);
-            setProd(data);
-        }
-        fetchprods();
+        dispatch(fetchProducts())
+        // const fetchprods = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products')
+        //     const data = await res.json();
+        //     console.log(data);
+        //     setProd(data);
+        // }
+        // fetchprods();
     }, [])
     const handleadd = (prod) => {
         dispatch(add(prod))
     }
-    return (
+    if (status === STATUSES.LOADING) {
+        return <h1>Loading...</h1>
+    } else if (status === STATUSES.ERROR) {
+        return <h1>Error</h1>
+    } else
+        return (
 
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4,1fr)' }}>
-            {
-                prod.map((prod) => (
-                    <div key={prod.id}>
-                        <img style={{ height: '200px', width: '200px' }} src={prod.image} alt="" />
-                        <p>{prod.title}</p>
-                        <p>{prod.price}</p>
-                        <button onClick={() => handleadd(prod)}>Add to cart</button>
-                    </div>
-                ))
-            }
-        </div>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4,1fr)' }}>
+                {
+                    data.map((prod) => (
+                        <div key={prod.id}>
+                            <img style={{ height: '200px', width: '200px' }} src={prod.image} alt="" />
+                            <p>{prod.title}</p>
+                            <p>{prod.price}</p>
+                            <button onClick={() => handleadd(prod)}>Add to cart</button>
+                        </div>
+                    ))
+                }
+            </div>
 
 
-    )
+        )
 }
 
 export default Products
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './CartSlice'
+import productReducer from './ProductSlice'
 const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        product: productReducer
     }
 });
 export default store;
